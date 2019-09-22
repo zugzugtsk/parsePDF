7214200d712403364454e2e42e14b3ae6d823de2
ZugZugtsk <zugzugtsk@gmail.com>
2019-09-22 19:54:15 +0700

    new

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # parsePDF
 This thing parses PDF for Youtube and GoogleDrive urls and dowloads the content of it.
 Can be a part of smth. bigger)
+Blah-blah changed locally
---
+++ new.py
@@ -0,0 +1,8 @@
+import requests                    # Прием/отправка запросов по HTTP.
+import re as re                    # Регулярки
+import io                          # input/output streams
+import pdfx                        # pdfx extracts urls from pdf documents
+from bs4 import BeautifulSoup #Beautifier
+import time
+from pprint import pprint
+from pytube import YouTube
